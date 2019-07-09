ac8785589ffe85f8b170bbf0143f78d7edc9d46c
Taffarel Xavier <taffarelxavier7@gmail.com>
2019-07-09 20:32:08 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+/* 
+ * AVALIAÇÃO I -  PROGRAMAÇÃO I
+ * INÍCIO ÀS 19H20
+ * File:   main.c
+ * Autor: Taffarel Xavier <taffarel_deus@hotmail.com>
+ *
+ * Criado em 25 de Abril de 2019, 19:14
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    printf("Okay");
+    
+    
+    return (EXIT_SUCCESS);
+}
+
